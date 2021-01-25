22fb1765d19ad8f60217d03c0340a5aa02eb1f51
AlexRogalskiy <alexander.rogalsky@yandex.ru>
2021-01-25 01:43:15 +0000

    Deploying to gh-pages from  @ 92c088c0bf90a23ec1efa77471a4be0bfe38c99e 🚀

---
+++ docs/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":drawingactivity:dokkaHtml/androidTestRelease": [], ":drawingactivity:dokkaHtml/debug": [], ":drawingactivity:dokkaHtml/main": [], ":drawingactivity:dokkaHtml/release": []}'
